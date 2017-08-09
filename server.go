254cd156be27c03013c5975dab7b7fe0bcbf7eb1
jconroy9 <james.conroy@macquarie.com>
2017-08-10 08:36:14 +1000

    Restructure to use Gorilla mux and then wrap handler in JWT authentication

--- server.go
+++ server.go
@@ -7,6 +7,12 @@ import (
 	"path"
 	"strconv"
 	"database/sql"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/handlers"
+	"os"
+	"github.com/auth0-community/auth0"
+	"fmt"
+	jose "gopkg.in/square/go-jose.v2"
 )
 
 type Post struct {
@@ -22,8 +28,6 @@ type Pet struct {
 }
 
 
-//var Db *sql.DB
-
 // connect to the Db
 func init() {
 	var err error
@@ -34,13 +38,44 @@ func init() {
 }
 
 func main() {
-	server := http.Server{
-		Addr: ":9088",
-	}
-	http.HandleFunc("/post/", handleRequest)
-	http.HandleFunc("/pet/", handlePetRequest)
-	http.HandleFunc("/pets", handlePetsRequest)
-	server.ListenAndServe()
+
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.Dir("./views/")))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+
+	r.Handle("/pet/{id}", authMiddleware(GetPetHandler)).Methods("GET")
+	r.Handle("/pets", authMiddleware(GetPetsHandler)).Methods("GET")
+	r.Handle("/pet", authMiddleware(PostPetHandler)).Methods("POST")
+
+	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
+
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// JC: these are from Auth0 management console
+		secret := []byte("pwsy9HvbACAKQYlw1Rp1EAL6ej2OfCZ3")
+		audience := "https://jamez.com"
+		issuer := "https://speak2jezza.au.auth0.com/"
+		secretProvider := auth0.NewKeyProvider(secret)
+
+		var audiences []string
+		configuration := auth0.NewConfiguration(secretProvider, append(audiences, audience), issuer, jose.HS256)
+		validator := auth0.NewValidator(configuration)
+
+		token, err := validator.ValidateRequest(r)
+
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println("Token is not valid:", token)
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
 }
 
 // main handler function
@@ -48,7 +83,7 @@ func handlePetRequest(w http.ResponseWriter, r *http.Request) {
 	var err error
 	switch r.Method {
 	case "GET":
-		err = handleGetPet(w, r)
+		//err = handleGetPet(w, r)
 	case "POST":
 		err = handlePostPet(w, r)
 	case "PUT":
@@ -62,82 +97,68 @@ func handlePetRequest(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func handlePetsRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
-	switch r.Method {
-	case "GET":
-		err = handleGetPets(w, r)
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
+var GetPetHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
-	switch r.Method {
-	case "GET":
-		err = handleGet(w, r)
-	case "POST":
-		err = handlePost(w, r)
-	case "PUT":
-		err = handlePut(w, r)
-	case "DELETE":
-		err = handleDelete(w, r)
-	}
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-}
 
-// Retrieve a post
-// GET /post/1
-func handleGet(w http.ResponseWriter, r *http.Request) (err error) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return
-	}
-	post, err := retrieve(id)
-	if err != nil {
-		return
-	}
-	output, err := json.MarshalIndent(&post, "", "\t\t")
-	if err != nil {
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
-	return
-}
-
-func handleGetPet(w http.ResponseWriter, r *http.Request) (err error) {
-	id, err := strconv.Atoi(path.Base(r.URL.Path))
-	if err != nil {
-		return
-	}
 	pet, err := retrievePet(id)
 	if err != nil {
 		return
 	}
+
 	output, err := json.MarshalIndent(&pet, "", "\t\t")
 	if err != nil {
 		return
 	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(output)
 	return
-}
+})
 
-func handleGetPets(w http.ResponseWriter, r *http.Request) (err error) {
-	//id, err := strconv.Atoi(path.Base(r.URL.Path))
-	//if err != nil {
-	//	return
-	//}
-	//TODO: how to read offset and limit from query string
-	offset := 1
-	limit := 3
+var PostPetHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	len := r.ContentLength
+	body := make([]byte, len)
+	r.Body.Read(body)
+	var pet Pet
+	json.Unmarshal(body, &pet)
+	err := pet.create()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(200)
+	return
+})
+
+var GetPetsHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	vals := r.URL.Query()
+	offsets :=  vals["offset"]
+
+	offset := 0
+	if offsets != nil {
+		offset1, err := strconv.Atoi(offsets[0])
+		if err != nil {
+			return
+		}
+		offset = offset1
+	}
+	limits :=  vals["limit"]
+
+	limit := 1
+	if limits != nil {
+		limit1, err := strconv.Atoi(limits[0])
+		if err != nil {
+			return
+		}
+		limit = limit1
+	}
+
 	pets, err := retrievePets(offset, limit)
 	if err != nil {
 		return
@@ -149,23 +170,12 @@ func handleGetPets(w http.ResponseWriter, r *http.Request) (err error) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(output)
 	return
-}
+
+})
+
 
 // Create a post
 // POST /post/
-func handlePost(w http.ResponseWriter, r *http.Request) (err error) {
-	len := r.ContentLength
-	body := make([]byte, len)
-	r.Body.Read(body)
-	var post Post
-	json.Unmarshal(body, &post)
-	err = post.create()
-	if err != nil {
-		return
-	}
-	w.WriteHeader(200)
-	return
-}
 
 func handlePostPet(w http.ResponseWriter, r *http.Request) (err error) {
 	len := r.ContentLength
---
+++ static/js/auth0-variables.js
@@ -0,0 +1,8 @@
+// var AUTH0_CLIENT_ID='YOUR-AUTH0-CLIENTID';
+var AUTH0_CLIENT_ID='dpQQK4I6v8QyfTZ9YwaaUuPi52uaWdjC';
+// var AUTH0_DOMAIN='YOUR-AUTH0-DOMAIN.auth0.com';
+var AUTH0_DOMAIN='speak2jezza.au.auth0.com';
+// var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_CALLBACK_URL=location.href;
+// var AUTH0_API_AUDIENCE='YOUR-AUTH0-API-AUDIENCE';
+var AUTH0_API_AUDIENCE='https://jamez.com';
